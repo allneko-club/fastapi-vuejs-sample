b202ce3e6229d18ac4fd63443336ddd6ef504db0
ToshikatsuTanahashi <allneko.club.llc@gmail.com>
2022-09-18 18:45:01 +0900

    不要ファイル削除

--- frontend/src/views/main/profile/ResetPassword.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>This is an reset password page</h1>
-</template>
-
-<style>
-
-</style>
--- frontend/src/views/main/profile/UserProfile.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>This is user profile page</h1>
-</template>
-
-<style>
-</style>
